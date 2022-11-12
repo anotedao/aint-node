731bcd2076784b2fd3f47fa766741ce9a80c5498
Pragmatic <cryptopragmatic@protonmail.com>
2022-11-12 10:04:30 +0100

    initial stuff

---
+++ helpers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"path"
+	"runtime"
+)
+
+func getCallerInfo() (info string) {
+
+	// pc, file, lineNo, ok := runtime.Caller(2)
+	_, file, lineNo, ok := runtime.Caller(2)
+	if !ok {
+		info = "runtime.Caller() failed"
+		return
+	}
+	// funcName := runtime.FuncForPC(pc).Name()
+	fileName := path.Base(file) // The Base function returns the last element of the path
+	return fmt.Sprintf("%s:%d: ", fileName, lineNo)
+}
+
+func logTelegram(message string) {
+	message = "aint-node:" + getCallerInfo() + url.PathEscape(url.QueryEscape(message))
+
+	_, err := http.Get(fmt.Sprintf("http://localhost:5002/log/%s", message))
+	if err != nil {
+		log.Println(err)
+	}
+}
